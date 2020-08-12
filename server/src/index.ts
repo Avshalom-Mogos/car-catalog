b1c40adeb07c24b3d2f39b888001f0515e627893
Avshalom Mogos <avshalomogos@gmail.com>
2020-08-12 20:07:22 +0300

    heroku deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import path from "path";
 import cars from "./data/cars.json";
 
 const app: Application = express();
@@ -7,5 +8,14 @@ const port: string | number = process.env.PORT || 5000;
 app.get("/cars", (req: Request, res: Response) => {
   res.send(cars);
 });
+
+//deployment
+if (process.env.NODE_ENV === "production") {
+  const root: string = path.join(__dirname, "..", "..", "client", "build");
+  app.use(express.static(root));
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", { root });
+  });
+}
 
 app.listen(port, () => console.log(`server is up on port:${port}`));
