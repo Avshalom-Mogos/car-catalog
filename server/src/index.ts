94865c8d63543c2f9b2f1248d110dda278d985fe
Avshalom Mogos <avshalomogos@gmail.com>
2020-08-12 18:38:12 +0300

    clean up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,11 @@
-import express, { Application, Request, Response } from 'express';
-import cars  from './data/cars.json'
-//@types/node UNINSTALL if there is no use
-
+import express, { Application, Request, Response } from "express";
+import cars from "./data/cars.json";
 
 const app: Application = express();
 const port: string | number = process.env.PORT || 5000;
 
-
-app.get('/cars', (req: Request, res: Response) => {
-    res.send(cars);
+app.get("/cars", (req: Request, res: Response) => {
+  res.send(cars);
 });
 
 app.listen(port, () => console.log(`server is up on port:${port}`));
