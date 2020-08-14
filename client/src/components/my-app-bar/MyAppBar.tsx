08fa8eceb80cedfc88d6e39ff8ed8d7b6d66698a
Avshalom Mogos <avshalomogos@gmail.com>
2020-08-14 18:51:19 +0300

    implemented michal comment #7

--- client/src/components/my-app-bar/MyAppBar.tsx
+++ client/src/components/my-app-bar/MyAppBar.tsx
@@ -1,25 +1,21 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import { useStyles } from "./useStyles";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { useStyles } from './useStyles';
 
 const MyAppBar = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Container maxWidth="md">
-          <Toolbar>
-            <Typography className={classes.title} variant="h6" noWrap>
-              Car Catalog
-            </Typography>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </div>
+    <AppBar className={classes.root} position='static' color='primary'>
+      <Container maxWidth='md'>
+        <Typography className={classes.title} variant='h5' noWrap>
+          Car Catalog
+        </Typography>
+      </Container>
+    </AppBar>
   );
 };
 export default MyAppBar;
--- client/src/components/my-app-bar/useStyles.ts
+++ client/src/components/my-app-bar/useStyles.ts
@@ -1,13 +1,16 @@
-import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      // border: '1px solid red',
+      height: 64,
     },
     title: {
-      flexGrow: 1,
-      display: "block",
+      paddingLeft: theme.spacing(5),
+      position: 'relative',
+      top: '50%',
     },
   })
 );
