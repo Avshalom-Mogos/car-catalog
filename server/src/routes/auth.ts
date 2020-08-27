c563d8c9c4e3e406918b3f33ea876044a18a21d8
Avshalom Mogos <avshalomogos@gmail.com>
2020-08-27 15:40:32 +0300

    backend refactor

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -10,63 +10,54 @@ authRouter.post('/signup', (req: Request, res: Response) => {
 
   //check if the user exist in the db
   User.findOne({ email }, (err, user) => {
-    if (err) return res.status(400).send(err);
-    if (!user) {
-      //hash password
-      bcrypt
-        .hash(password, 10)
-        .then(hashedPassword => {
-          //create user
-          const user = new User({
-            name,
-            email,
-            authProvider,
-            password: hashedPassword,
-          });
-          user
-            .save()
-            .then(newUser => res.status(201).send(newUser))
-            .catch(err => res.status(400).send(err));
-        })
-        .catch(err => res.status(400).send(err));
-    } else {
+    if (err) return res.status(500).send('something went wrong');
+    if (user) {
       return res.status(400).send('This email is taken by another account');
     }
+
+    //hash password
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) return res.status(500).send('something went wrong');
+      //  create user
+      const user = new User({
+        name,
+        email,
+        authProvider,
+        password: hashedPassword,
+      });
+      user.save((err, newUser) => {
+        if (err) return res.status(500).send('something went wrong');
+        res.status(201).send(newUser);
+      });
+    });
   });
 });
 
 authRouter.post('/signin', (req, res) => {
   const { email, password } = req.body;
   //check if the user exists in the db
-  User.findOne({ email }, (err, user) => {
-    if (err) return res.status(400).send(err);
-    if (user) {
-      const { name, _id, email, authProvider } = user;
+  User.findOne({ email }, (err, userFound) => {
+    if (err) return res.status(500).send('something went wrong');
+    if (!userFound) return res.status(400).send('incorrect email or passowrd');
 
-      if (authProvider !== 'myApp')
-        return res
-          .status(403)
-          .send(`You can't use this login method with this email`);
-      //check if password is correct
-      bcrypt
-        .compare(password, user.password as string)
-        .then(result => {
-          if (result) {
-            res.status(200).send({
-              _id,
-              name,
-              email,
-              authProvider,
-              token: createToken(_id),
-            });
-          } else {
-            return res.status(403).send('incorrect password or email');
-          }
-        })
-        .catch(err => res.status(500).send('something went wrong'));
-    } else {
-      return res.status(400).send('incorrect password or email');
+    const { name, _id, email, authProvider } = userFound;
+    if (authProvider !== 'myApp') {
+      return res.status(403).send(`Wrong login method`);
     }
+
+    //check if password is correct
+    bcrypt.compare(password, userFound.password as string, (err, same) => {
+      if (err) return res.status(500).send('something went wrong');
+      if (!same) return res.status(403).send('incorrect email or passowrd');
+      //send ther user
+      res.status(200).send({
+        _id,
+        name,
+        email,
+        authProvider,
+        token: createToken(_id),
+      });
+    });
   });
 });
 
@@ -81,8 +72,9 @@ authRouter.post('/soical', (req: Request, res: Response) => {
         .status(200)
         .send({ ...userFound._doc, token: createToken(userFound._id) });
     } else {
-      //  if not found create user in the db and return it
-      new User(user).save().then(newUser => {
+      //  if not found create user in the db and
+      new User(user).save((err, newUser) => {
+        if (err) return res.status(400).send(err);
         res
           .status(201)
           .send({ ...newUser._doc, token: createToken(newUser._id) });
--- server/src/routes/cars.ts
+++ server/src/routes/cars.ts
@@ -3,12 +3,10 @@ import validateToken from '../middlewares/validateToken';
 import Car from '../schemas/Car';
 
 const carsRouter: Router = express.Router();
-carsRouter.get('/', validateToken, async (req: Request, res: Response) => {
-  try {
-    const cars = await Car.find();
+carsRouter.get('/', validateToken, (req: Request, res: Response) => {
+  Car.find((err, cars) => {
+    if (err) return res.status(500).send('something went wrong');
     res.status(200).send(cars);
-  } catch (error) {
-    res.status(500).send('something went wrong');
-  }
+  });
 });
 export default carsRouter;
